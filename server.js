d62ddac2563b11fc51143d390e716e038beeda9f
Caleb Confer <calebconfer@Calebs-MacBook-Air.local>
2021-06-14 11:33:09 -0400

    server boilerplate and mongodb connected

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+//DEPENDENCIES=========================================
+const { config } = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const app = express();
+const port = process.env.PORT || 3000;
+//CONFIGURE MONGOOSE===================================
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+// -   -   -   -   -   -   -   -   -   -   -   -   -   -
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+//set the default view engine =========================
+app.set('view engine', 'ejs');
+//HOME ROUTE ==========================================
+app.get('/', (req, res) => {
+    res.send('welcome home');
+});
+//MOUNTING MIDDLEWARE =================================
+app.use(express.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
+
+// link to controllers -    -   -   -   -   -   -   -   
+
+
+//LISTENER============================================
+app.listen(port, () => {
+    console.log('Express is listening on port:', port);
+});
